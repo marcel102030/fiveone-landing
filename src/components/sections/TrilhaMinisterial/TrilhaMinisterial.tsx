bf28dc9bc3ca6292be9cae3d0a3a320c26c95ef0
Marcelo Junior da Silva <marcelo.dasilva@gsoftware.com.br>
2025-05-28 13:24:06 -0300

    TrilharMinisterial2

--- src/components/sections/TrilhaMinisterial/TrilhaMinisterial.tsx
+++ src/components/sections/TrilhaMinisterial/TrilhaMinisterial.tsx
@@ -11,6 +11,7 @@ interface TrilhaCard {
   title: string;
   description: string;
   alt: string;
+  link: string;
 }
 
 const trilhas: TrilhaCard[] = [
@@ -20,6 +21,7 @@ const trilhas: TrilhaCard[] = [
     description:
       "Nesta trilha exploraremos o apostólico. Das raízes teológicas ao chamado individual.",
     alt: "Apóstolo",
+    link: "https://escolafiveone.hotmart.host/formacao-de-mestre-five-one-df44d8cd-3a6b-44b0-aaec-652290fc529a",
   },
   {
     icon: profetaIcon,
@@ -27,6 +29,7 @@ const trilhas: TrilhaCard[] = [
     description:
       "Nesta trilha exploraremos o profético. Das raízes teológicas ao chamado individual.",
     alt: "Profeta",
+    link: "https://escolafiveone.hotmart.host/formacao-de-mestre-five-one-df44d8cd-3a6b-44b0-aaec-652290fc529a",
   },
   {
     icon: evangelistaIcon,
@@ -34,6 +37,7 @@ const trilhas: TrilhaCard[] = [
     description:
       "Nesta trilha exploraremos o evangelístico. Das raízes teológicas ao chamado individual.",
     alt: "Evangelista",
+    link: "https://escolafiveone.hotmart.host/formacao-de-mestre-five-one-df44d8cd-3a6b-44b0-aaec-652290fc529a",
   },
   {
     icon: pastorIcon,
@@ -41,6 +45,7 @@ const trilhas: TrilhaCard[] = [
     description:
       "Nesta trilha exploraremos o pastoral. Das raízes teológicas ao chamado individual.",
     alt: "Pastor",
+    link: "https://escolafiveone.hotmart.host/formacao-de-mestre-five-one-df44d8cd-3a6b-44b0-aaec-652290fc529a",
   },
   {
     icon: mestreIcon,
@@ -48,6 +53,7 @@ const trilhas: TrilhaCard[] = [
     description:
       "Nesta trilha exploraremos o ensino. Das raízes teológicas ao chamado individual.",
     alt: "Mestre",
+    link: "https://escolafiveone.hotmart.host/formacao-de-mestre-five-one-df44d8cd-3a6b-44b0-aaec-652290fc529a",
   },
 ];
 
@@ -58,13 +64,19 @@ const TrilhaMinisterial = () => {
         <h2>ESCOLHA A SUA TRILHA MINISTERIAL</h2>
         <div className="trilha-cards">
           {trilhas.map((trilha, index) => (
-            <div key={index} className="trilha-card">
+            <a
+              key={index}
+              className="trilha-card"
+              href={trilha.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div className="card-icon">
                 <img src={trilha.icon} alt={trilha.alt} />
               </div>
               <h3>{trilha.title}</h3>
               <p>{trilha.description}</p>
-            </div>
+            </a>
           ))}
         </div>
       </div>
